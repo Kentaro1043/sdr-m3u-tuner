3049c6a700e84e8c72424e763a2253d932391238
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-05-31 00:19:12 +0900

    🐛 fix

--- gnuradio/fm_reception.py
+++ gnuradio/fm_reception.py
@@ -9,8 +9,6 @@
 # Author: Kentaro1043
 # GNU Radio version: 3.10.12.0
 
-from PyQt5 import Qt
-from gnuradio import qtgui
 from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import filter
@@ -19,7 +17,6 @@ from gnuradio import gr
 from gnuradio.fft import window
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
@@ -29,60 +26,27 @@ import threading
 
 
 
-class fmrx(gr.top_block, Qt.QWidget):
+
+class fm_reception(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "FM reception", catch_exceptions=True)
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("FM reception")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except BaseException as exc:
-            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "fmrx")
-
-        try:
-            geometry = self.settings.value("geometry")
-            if geometry:
-                self.restoreGeometry(geometry)
-        except BaseException as exc:
-            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
         self.flowgraph_started = threading.Event()
 
         ##################################################
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 48000
-        self.freq = freq = 80e6
 
         ##################################################
         # Blocks
         ##################################################
 
-        self._freq_msgdigctl_win = qtgui.MsgDigitalNumberControl(lbl='', min_freq_hz=70e6, max_freq_hz=100e6, parent=self, thousands_separator=",", background_color="black", fontColor="white", var_callback=self.set_freq, outputmsgname='freq')
-        self._freq_msgdigctl_win.setValue(80e6)
-        self._freq_msgdigctl_win.setReadOnly(False)
-        self.freq = self._freq_msgdigctl_win
-
-        self.top_layout.addWidget(self._freq_msgdigctl_win)
         self.osmosdr_source_0 = osmosdr.source(
             args="numchan=" + str(1) + " " + "hackrf=0"
         )
         self.osmosdr_source_0.set_sample_rate((samp_rate*50))
-        self.osmosdr_source_0.set_center_freq( freq, 0)
+        self.osmosdr_source_0.set_center_freq(80M, 0)
         self.osmosdr_source_0.set_freq_corr(0, 0)
         self.osmosdr_source_0.set_dc_offset_mode(0, 0)
         self.osmosdr_source_0.set_iq_balance_mode(0, 0)
@@ -119,20 +83,11 @@ class fmrx(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.freq, 'valueout'), (self.freq, 'valuein'))
-        self.connect((self.analog_wfm_rcv_pll_0, 0), (self.blocks_wavfile_sink_0, 0))
         self.connect((self.analog_wfm_rcv_pll_0, 1), (self.blocks_wavfile_sink_0, 1))
+        self.connect((self.analog_wfm_rcv_pll_0, 0), (self.blocks_wavfile_sink_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_pll_0, 0))
         self.connect((self.osmosdr_source_0, 0), (self.low_pass_filter_0, 0))
 
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "fmrx")
-        self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
-        event.accept()
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -142,41 +97,31 @@ class fmrx(gr.top_block, Qt.QWidget):
         self.low_pass_filter_0.set_taps(firdes.low_pass(1, (self.samp_rate*50), 250e3, 50e3, window.WIN_HAMMING, 6.76))
         self.osmosdr_source_0.set_sample_rate((self.samp_rate*50))
 
-    def get_freq(self):
-        return self.freq
-
-    def set_freq(self, freq):
-        self.freq = freq
-        self.osmosdr_source_0.set_center_freq( self.freq, 0)
 
 
 
-
-def main(top_block_cls=fmrx, options=None):
-
-    qapp = Qt.QApplication(sys.argv)
-
+def main(top_block_cls=fm_reception, options=None):
     tb = top_block_cls()
-
-    tb.start()
-    tb.flowgraph_started.set()
-
-    tb.show()
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
         tb.wait()
 
-        Qt.QApplication.quit()
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
+    tb.start()
+    tb.flowgraph_started.set()
 
-    qapp.exec_()
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
 
 if __name__ == '__main__':
     main()
