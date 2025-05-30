52fb68dd6aac7d892133a63c86686f7f5167ea35
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-05-31 00:43:02 +0900

    ✨ add gnu radio block for test

--- gnuradio/fm_reception.py
+++ gnuradio/fm_reception.py
@@ -9,19 +9,15 @@
 # Author: Kentaro1043
 # GNU Radio version: 3.10.12.0
 
-from gnuradio import analog
 from gnuradio import blocks
-from gnuradio import filter
-from gnuradio.filter import firdes
 from gnuradio import gr
+from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import osmosdr
-import time
 import threading
 
 
@@ -42,51 +38,26 @@ class fm_reception(gr.top_block):
         # Blocks
         ##################################################
 
-        self.osmosdr_source_0 = osmosdr.source(
-            args="numchan=" + str(1) + " " + "hackrf=0"
-        )
-        self.osmosdr_source_0.set_sample_rate((samp_rate*50))
-        self.osmosdr_source_0.set_center_freq(80M, 0)
-        self.osmosdr_source_0.set_freq_corr(0, 0)
-        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
-        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
-        self.osmosdr_source_0.set_gain_mode(False, 0)
-        self.osmosdr_source_0.set_gain(50, 0)
-        self.osmosdr_source_0.set_if_gain(20, 0)
-        self.osmosdr_source_0.set_bb_gain(20, 0)
-        self.osmosdr_source_0.set_antenna('', 0)
-        self.osmosdr_source_0.set_bandwidth(1e6, 0)
-        self.low_pass_filter_0 = filter.fir_filter_ccf(
-            5,
-            firdes.low_pass(
-                1,
-                (samp_rate*50),
-                250e3,
-                50e3,
-                window.WIN_HAMMING,
-                6.76))
+        self.blocks_wavfile_source_0 = blocks.wavfile_source('../tmp/test.wav', True)
         self.blocks_wavfile_sink_0 = blocks.wavfile_sink(
-            '../tmp/output.wav',
+            '../tmp/sound.wav',
             2,
             samp_rate,
             blocks.FORMAT_WAV,
             blocks.FORMAT_PCM_16,
             False
             )
-        self.analog_wfm_rcv_pll_0 = analog.wfm_rcv_pll(
-        	demod_rate=(samp_rate*10),
-        	audio_decimation=10,
-        	deemph_tau=(75e-6),
-        )
+        self.blocks_throttle2_1 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_wfm_rcv_pll_0, 1), (self.blocks_wavfile_sink_0, 1))
-        self.connect((self.analog_wfm_rcv_pll_0, 0), (self.blocks_wavfile_sink_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_pll_0, 0))
-        self.connect((self.osmosdr_source_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.blocks_wavfile_sink_0, 0))
+        self.connect((self.blocks_throttle2_1, 0), (self.blocks_wavfile_sink_0, 1))
+        self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.blocks_wavfile_source_0, 1), (self.blocks_throttle2_1, 0))
 
 
     def get_samp_rate(self):
@@ -94,8 +65,8 @@ class fm_reception(gr.top_block):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, (self.samp_rate*50), 250e3, 50e3, window.WIN_HAMMING, 6.76))
-        self.osmosdr_source_0.set_sample_rate((self.samp_rate*50))
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle2_1.set_sample_rate(self.samp_rate)
 
 
 
